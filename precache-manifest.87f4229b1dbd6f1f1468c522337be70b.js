c695dc62703681ad17cb1e11ce4ad9edf718d40f
sebastnhr <sebastn24@gmail.com>
2020-09-20 10:32:42 -0300

    Updates

---
+++ precache-manifest.87f4229b1dbd6f1f1468c522337be70b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce79d719ddef797926dbd858b0211a04",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "fbba4e83c3587c71936c",
+    "url": "/portfolio/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "c8366ef86b38081951ba",
+    "url": "/portfolio/static/css/main.019f1f4b.chunk.css"
+  },
+  {
+    "revision": "fbba4e83c3587c71936c",
+    "url": "/portfolio/static/js/2.0b22a555.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.0b22a555.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8366ef86b38081951ba",
+    "url": "/portfolio/static/js/main.de7f09c7.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "00eb873969bb30fa391de31bca823310",
+    "url": "/portfolio/static/media/bg-image.00eb8739.jpg"
+  },
+  {
+    "revision": "ace43bcd92890886fd0a2e3ebc5e823a",
+    "url": "/portfolio/static/media/contact.ace43bcd.png"
+  }
+]);
