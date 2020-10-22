51f2992937fb79829447b74433441d7e1cf0c304
sebastnhr <sebastn24@gmail.com>
2020-10-22 19:09:28 -0300

    Updates

---
+++ precache-manifest.82cc1efa0bc84492c14b2e4f260c756f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "556d879cba291d6cf596293cf1bef75e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4785532438b8fc02b5bd",
+    "url": "/portfolio/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "303cfcd8b0abe11e3e93",
+    "url": "/portfolio/static/css/main.8775929f.chunk.css"
+  },
+  {
+    "revision": "4785532438b8fc02b5bd",
+    "url": "/portfolio/static/js/2.c911da94.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.c911da94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "303cfcd8b0abe11e3e93",
+    "url": "/portfolio/static/js/main.f7fbd5b8.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "00eb873969bb30fa391de31bca823310",
+    "url": "/portfolio/static/media/bg-image.00eb8739.jpg"
+  },
+  {
+    "revision": "ace43bcd92890886fd0a2e3ebc5e823a",
+    "url": "/portfolio/static/media/contact.ace43bcd.png"
+  },
+  {
+    "revision": "ae56997fe7b0fd81085649bd50d0451c",
+    "url": "/portfolio/static/media/tatuajesctm.ae56997f.jpg"
+  },
+  {
+    "revision": "0798bab54737e37aae7aac63d0615e5e",
+    "url": "/portfolio/static/media/tatuajesctmPage.0798bab5.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.09260931116866a1d0e9268841802d7e.js"
+  "/portfolio/precache-manifest.82cc1efa0bc84492c14b2e4f260c756f.js"
 );
 
 self.addEventListener('message', (event) => {
